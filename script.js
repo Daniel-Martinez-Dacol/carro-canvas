6ba546751c0f3aa6799115efbf1abac89d0c2753
daniel-martinez <danieldaol99@gmail.com>
2022-09-21 23:10:19 -0300

    Carrionho Vrum Vruumm beep beep

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var canvas = document.getElementById("tela");
 //Recupera o contexto de desenho bidimensional do canvas
 var ctx = canvas.getContext("2d"); //Não utilizado em WebGL
 var x = 200, y = 100, larg = 10, alt = 10, ang = 0;
-var x2 = 100, y2 = 200, ang2 = 0, larg2 = 100, alt2 = 50;
+var x2 = 200, y2 = 200, ang2 = 0, larg2 = 200, alt2 = 100;
 
 function desenharTriangulo() {
     ctx.beginPath();
@@ -24,26 +24,64 @@ function desenharCirculo() {
 
 function desenhar() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = "rgb(255, 128, 0)";
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 3;
+
     //retângulo maior
     ctx.save();
-        ctx.translate(x2,y2);
+    ctx.translate(x2,y2);
         ctx.rotate(ang2);
-        ctx.fillStyle = "rgb(200, 200, 0)";
-        ctx.fillRect(-larg2/2,-alt2/2,larg2,alt2);
-    ctx.restore();
-    //ponteiro
-    ctx.save();
-        ctx.translate(x, y);
-        ctx.rotate(ang);
+        
+        // RODAS :
+        //ctx.fillStyle = "rgb(120, 120, 120)";
+        //ctx.fillRect(larg2-150, alt2-60,60, 35);
+        //ctx.beginPath();
+        //ctx.fillRect(larg2-150, alt2-180,60, 35);
+        //ctx.beginPath();
+        //ctx.fillRect(larg2-310, alt2-180,60, 35);
+        //ctx.beginPath();
+        //ctx.fillRect(larg2-310, alt2-60,60, 35);
+        //ctx.beginPath();
+
+        // Farol
         ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(50, 0);
+        ctx.moveTo(larg2-215,alt2-104);
+        ctx.lineTo(250,120);
+        ctx.lineTo(250,-120);
+        ctx.fillStyle = "rgb(255, 128, 0)";
+        ctx.fill();
+
         ctx.stroke();
-        ctx.fillStyle = "rgb(0, 0, 200)";
-        ctx.fillRect(-larg/2, -alt/2, larg, alt);
+        ctx.fillStyle = "rgb(176, 176, 176)";
+        ctx.fillRect(-larg2/2,-alt2/2,larg2,alt2);
+
+
     ctx.restore();
+    //rodas
+    ctx.save();
+    ctx.translate(x2,y2);
+    ctx.rotate(ang);
+        // RODAS :
+        ctx.fillStyle = "rgb(120, 120, 120)";
+        ctx.fillRect(larg2-150, alt2-60,60, 35);
+        ctx.beginPath();
+        ctx.fillRect(larg2-150, alt2-180,60, 35);
+        ctx.beginPath();
+        ctx.fillRect(larg2-310, alt2-180,60, 35);
+        ctx.beginPath();
+        ctx.fillRect(larg2-310, alt2-60,60, 35);
+        ctx.beginPath();
+    ctx.restore();    
+    ctx.save();
+
+       // ctx.translate(x, y);
+        //ctx.rotate(ang2);
+        //ctx.beginPath();
+           // ctx.moveTo(0, 0);
+            //ctx.lineTo(50, 0);
+        //ctx.stroke();
+      // ctx.fillStyle = "rgb(120, 0, 200)";
+      //ctx.fillRect(0, 0,40, 40);
+    //ctx.restore();
     requestAnimationFrame(desenhar);
 }
 
@@ -68,6 +106,14 @@ function desenhar() {
     //y += 1;
     //desenharCirculo();
     requestAnimationFrame(desenhar);
+
+                //farol kkkkkkk
+            ctx.moveTo(larg2/2,alt2/2);
+            ctx.lineTo(larg2,alt2);
+            ctx.lineTo(larg2,alt2-200);
+            ctx.lineTo(larg2-100,alt2 - 150);
+            ctx.lineTo(larg2/2,alt2/2);
+            ctx.strokeStyle = "rgb(255, 128, 0)";
 }*/
 
 /*function desenhar() {
@@ -128,9 +174,11 @@ document.onkeydown = function  (evt) {
             break;
         case 81: //Q
             ang2 -= 0.1;
+            ang -= 0.1;
             break;
         case 69: //E
             ang2 += 0.1;
+            ang += 0.1;
             break;
     }
 }
